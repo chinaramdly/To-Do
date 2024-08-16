31334544654c169735f68f81956ce086537e29a4
chinara mammadova <cmmmdova739@gmail.com>
2024-08-16 20:15:37 +0000

    input

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 "use strict";
 $(document).ready(function () {
 
-    $('.button').click(function (e) { 
+    $('.btn').click(function (e) {
         e.preventDefault();
-        let b= (a)=>{
-            for( let i=1; i<=a; i++) {
-                $('.textbox').append(`<div class="btn">${'salam'}</div><div class="btn">${'salam salam salam'}</div>`);
+        let b = (a) => {
+            for (let i = 1; i <= a; i++) {
+                $('.textbox').append(`<div class="btn">${a}</div>`);
+
             }
         }
         b($('input').val());
