b686e68f037061cc242a7369eb465a650c282589
chinara mammadova <cmmmdova739@gmail.com>
2024-08-16 11:56:00 +0000

    textbox

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        let b= (a)=>{
+            for( let i=1; i<=a; i++) {
+                $('.textbox').append(`<h2>${'5f'}<h2>`);
+            }
+        }
+        b($('input').val());
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 });
