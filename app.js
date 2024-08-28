293db104352dda8d84f8203f41d4e56c00f9fbe6
chinara mammadova <cmmmdova739@gmail.com>
2024-08-28 20:13:46 +0000

    minibox

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 "use strict";
 $(document).ready(function () {
 
+
     $('.btn').click(function (e) {
         e.preventDefault();
-        let b = (a) => {
-            for (let i = 1; i <= a; i++) {
-                $('.textbox').append(`<div class="btn">${i}</div>`);
-
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`) }
             }
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val())
         }
-        b($('input').val());
+         $('input').val('')
     });
 
 
@@ -24,3 +26,15 @@ $(document).ready(function () {
 
 
 });
+
+
+// $('.btn').click(function (e) {
+//     e.preventDefault();
+//     let b = (a) => {
+//         for (let i = 1; i <= a; i++) {
+//             $('.minibox').append(`<div class="btn">${i}</div>`);
+
+//         }
+//     }
+//     b($('input').val());
+// });
